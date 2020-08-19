7ec1d18ee4011de7927c67ebd412bbd96f59786e
gabrielbohannon <69912740+gabrielbohannon@users.noreply.github.com>
2020-08-19 11:33:20 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,20 @@
+/********************************************************
+ * Java Program Name
+ ********************************************************
+ * Student Name (first and last)
+ * Date
+ * Course Number and Section
+ *******************************************************/
+
+ public class Hello {
+	
+	public static void main(String[] args) {
+
+	  System.out.println("<Student Name>");
+	  System.out.println("<Java Project Name>");
+	  System.out.println("<Project Description>");
+	  System.out.println("<Version Date>");
+	  System.out.println("<Course Number and Section>");
+	  System.out.println();
+   	}
+ }
